430caeb856d65bdfffa78f0982f6383b4e110d1a
OlimpiaDev16 <168941945+OlimpiaDev16@users.noreply.github.com>
2025-07-28 17:17:02 -0400

    Initial Commit

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Olimpia Compean on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
